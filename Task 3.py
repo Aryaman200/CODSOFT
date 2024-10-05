fafde62bab2c6a5ff233fe38cd74187052634b23
Aryaman200 <133807098+Aryaman200@users.noreply.github.com>
2024-10-05 11:15:31 +0530

    Update Task 3.py

--- Task 3.py
+++ Task 3.py
@@ -1,46 +1,49 @@
 import string
 import random
 
-# Getting password length
-length = int(input("Enter password length: "))
+# Get desired password length from the user
+password_length = int(input("Enter the desired password length: "))
 
-print('''Choose character set for password from these : 
-		1. Digits
-		2. Letters
-		3. Special characters
-		4. Exit''')
+# Display character set options
+print('''Select the character set to include in the password:
+        1. Letters (Uppercase & Lowercase)
+        2. Numbers (Digits)
+        3. Special characters
+        4. Done selecting (Generate password)
+''')
 
-characterList = ""
+# Initialize a variable to hold all selected character types
+available_characters = ""
 
-# Getting character set for password
-while(True):
-	choice = int(input("Pick a number "))
-	if(choice == 1):
-		
-		# Adding letters to possible characters
-		characterList += string.ascii_letters
-	elif(choice == 2):
-		
-		# Adding digits to possible characters
-		characterList += string.digits
-	elif(choice == 3):
-		
-		# Adding special characters to possible
-		# characters
-		characterList += string.punctuation
-	elif(choice == 4):
-		break
-	else:
-		print("Please pick a valid option!")
+# Loop to select character sets
+while True:
+    selection = input("Enter your option (1/2/3/4): ")
 
-password = []
+    if selection == '1':
+        available_characters += string.ascii_letters
+        print("Letters added to the character set.")
 
-for i in range(length):
+    elif selection == '2':
+        available_characters += string.digits
+        print("Numbers added to the character set.")
 
-	# Picking a random character from our 
-	# character list
-	randomchar = random.choice(characterList)
-	password.append(randomchar)
+    elif selection == '3':
+        available_characters += string.punctuation
+        print("Special characters added to the character set.")
 
-# printing password as a string
-print("The random password is " + "".join(password))
+    elif selection == '4':
+        if available_characters:
+            print("Generating your password...")
+            break
+        else:
+            print("No character set selected. Please choose at least one option.")
+
+    else:
+        print("Invalid option. Please select a valid number (1/2/3/4).")
+
+# Generate the random password
+if available_characters:
+    password = ''.join(random.choice(available_characters) for _ in range(password_length))
+    print(f"Your randomly generated password is: {password}")
+else:
+    print("Password generation failed. No valid character set was selected.")
